211e3d860352633fbd962110aa94caf7758d993c
Divyanshi-Bansal <divyanshibansal9@gmail.com>
2024-09-16 17:25:40 +0530

    first way of intercepted routes by using (.) prefix of same folder name

---
+++ src/app/f1/(.)f2/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const InterceptedF2Route = () => {
+  return (
+    <div>
+      <h1>(.) InterceptedF2Route page</h1>
+    </div>
+  )
+}
+
+export default InterceptedF2Route
---
+++ src/app/f1/f2/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const F2 = () => {
+  return (
+    <div>
+      <h1>F2 page</h1>
+    </div>
+  )
+}
+
+export default F2
