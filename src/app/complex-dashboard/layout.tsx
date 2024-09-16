a17cc8ddac857effb9d01fa553817613406d141e
Divyanshi-Bansal <divyanshibansal9@gmail.com>
2024-09-16 11:23:39 +0530

    conditional routes

--- src/app/complex-dashboard/layout.tsx
+++ src/app/complex-dashboard/layout.tsx
@@ -1,27 +1,39 @@
 "use client";
 import React from 'react'
 
-const ComplexDashboardLayout = ({children, users, revenue, notification}: Readonly<{
+const ComplexDashboardLayout = ({children, users, revenue, notification, login}: Readonly<{
     children: React.ReactNode;
     users: React.ReactNode;
     revenue: React.ReactNode;
-    notification: React.ReactNode
+    notification: React.ReactNode;
+    login: React.ReactNode
 }>) => {
     // by adding @before folder name, so basically we can't go to that folder's page by route url
     // but we can add these components in layout : parallel routes
+
+    const isUserLoggedIn = false;
   return (
-    <div>
-      <div>
-        {children}
-      </div>
-      <div style={{display:"flex"}}>
-        <div style={{display:"flex", flexDirection:"column"}}>
-            <div>{users}</div>
-            <div>{revenue}</div>
+    <>
+    { isUserLoggedIn ?
+        <div>
+            <div>
+                {children}
+            </div>
+            <div style={{display:"flex"}}>
+            <div style={{display:"flex", flexDirection:"column"}}>
+                <div>{users}</div>
+                <div>{revenue}</div>
+            </div>
+            <div style={{display:"flex"}}>{notification}</div>
+            {login}
+            </div>
         </div>
-        <div style={{display:"flex"}}>{notification}</div>
-      </div>
-    </div>
+        :
+       <div>
+         {login}
+       </div>
+    }
+    </>
   )
 }
 
