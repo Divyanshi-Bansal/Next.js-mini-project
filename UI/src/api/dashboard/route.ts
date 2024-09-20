2ffdc32571d0b425bcf82b9cc6f629933b80f5ca
Divyanshi-Bansal <divyanshibansal9@gmail.com>
2024-09-20 12:31:01 +0530

    Split project into UI and Backend. (just made a new folder)

---
+++ UI/src/api/dashboard/route.ts
@@ -0,0 +1,6 @@
+export async function GET(){
+    return new Response("fet response from dashboard route");
+}
+export async function POST(){
+    return new Response("fet response from dashboard route");
+}
