15d053f733403dd666f307d7cc6011546aaa200b
Divyanshi-Bansal <divyanshibansal9@gmail.com>
2024-09-20 19:12:48 +0530

    cookie in headers

--- backend/src/app/api/profile/route.ts
+++ backend/src/app/api/profile/route.ts
@@ -1,4 +1,4 @@
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest){
@@ -8,5 +8,19 @@ export async function GET(request: NextRequest){
     const requestSecHeaders = headers();//second way
     console.log("second way of auth: -", requestSecHeaders.get("Authorization"));
     console.log("auth--- of profile api--", auth);
-    return new Response("Profile API GET data");
+
+    // cookies- 1st way
+    const themeCookie = request.cookies.get("theme");
+    console.log("theme cookie--:", themeCookie);
+
+    cookies().set("resultsPerPage", "20");
+    console.log(cookies().get("resultsPerPage"));
+    // cookies().delete
+
+    return new Response("Profile API GET data", {
+        headers:{
+            "Content-Type":"text/html",
+            "Set-Cookie":"theme=dark"
+        }
+    });
 }
