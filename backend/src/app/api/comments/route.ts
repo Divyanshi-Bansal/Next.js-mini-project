6bd416f3d46028c02a1914fdf71736fcee65f753
Divyanshi-Bansal <divyanshibansal9@gmail.com>
2024-09-20 18:45:37 +0530

    url query parameters: by using nextUrl and NextRequest

--- backend/src/app/api/comments/route.ts
+++ backend/src/app/api/comments/route.ts
@@ -1,12 +1,13 @@
+import { NextRequest } from "next/server";
 import { comments } from "./data";
 
 type RequestData = {
     text:string
 }
 
-export async function GET(){
-    return Response.json(comments);
-}
+// export async function GET(){
+//     return Response.json(comments);
+// }
 
 // export async function POST(data: RequestData){
 //     return new Response(`Request is successful data: ${data.text}`);
@@ -26,4 +27,18 @@ export async function POST(request: Request){
         },
         status:201
     })
+}
+
+export async function GET(request: NextRequest){
+    const searchParams = request.nextUrl.searchParams;
+    const word = searchParams.get("word");
+    if(word != undefined && word != null && word != ""){
+        const filteredComments = comments.filter(comment => comment.text.includes(word));
+        if(filteredComments.length > 0){
+            return Response.json(filteredComments);
+        }
+    }else if(word == undefined || word == null){
+        return Response.json(comments);
+    }
+    return new Response("no record found!");
 }
